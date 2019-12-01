00fc1d426c3b0e3f313a45d098750627ba35ff67
han45678 <han0910812144@gmail.com>
2019-12-02 00:30:47 +0800

    new
    改壞了 重傳

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,20 @@
-$(function () {
+$(document).ready(function () {
     $.ajax({
         method: "GET",
         url: "../HanWorks/js/data.json"
     })
-        .done(function (data) {
-            var position = data.position;
-            var englishName = data.englishName;
-            var Name = data.Name;
-            var birthday = data.birthday;
-            var expertise = data.expertise;
-            var phone = data.phone;
-            var email = data.email;
-            
-            // var works0 = data.works[0].worksPhoto;
-            // var works1 = data.works[1].worksPhoto;
-            // var works2 = data.works[2].worksPhoto;
-            // var works3 = data.works[3].worksPhoto;
+        .done(function (msg) {
+            var position = msg.results[0].position;
+            var englishName = msg.results[0].englishName;
+            var Name = msg.results[0].Name;
+            var birthday = msg.results[0].birthday;
+            var expertise = msg.results[0].expertise;
+            var phone = msg.results[0].phone;
+            var email = msg.results[0].email;
+            var works0 = msg.results[0].works[0].worksPhoto;
+            var works1 = msg.results[0].works[1].worksPhoto;
+            var works2 = msg.results[0].works[2].worksPhoto;
+            var works3 = msg.results[0].works[3].worksPhoto;
 
             $("span.englishName,h4.englishName").append(englishName);
             $("h3.position").append(position);
