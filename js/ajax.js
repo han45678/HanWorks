ff446af9a6c7812ff0093d66d3f115908d5ef165
han45678 <han0910812144@gmail.com>
2019-12-02 00:20:36 +0800

    修改
    修改多餘的東西

--- js/ajax.js
+++ js/ajax.js
@@ -1,20 +1,21 @@
-$(document).ready(function () {
+$(function () {
     $.ajax({
         method: "GET",
         url: "../HanWorks/js/data.json"
     })
-        .done(function (msg) {
-            var position = msg.results[0].position;
-            var englishName = msg.results[0].englishName;
-            var Name = msg.results[0].Name;
-            var birthday = msg.results[0].birthday;
-            var expertise = msg.results[0].expertise;
-            var phone = msg.results[0].phone;
-            var email = msg.results[0].email;
-            var works0 = msg.results[0].works[0].worksPhoto;
-            var works1 = msg.results[0].works[1].worksPhoto;
-            var works2 = msg.results[0].works[2].worksPhoto;
-            var works3 = msg.results[0].works[3].worksPhoto;
+        .done(function (data) {
+            var position = data.results[0].position;
+            var englishName = data.results[0].englishName;
+            var Name = data.results[0].Name;
+            var birthday = data.results[0].birthday;
+            var expertise = data.results[0].expertise;
+            var phone = data.results[0].phone;
+            var email = data.results[0].email;
+            
+            var works0 = data.results[0].works[0].worksPhoto;
+            var works1 = data.results[0].works[1].worksPhoto;
+            var works2 = data.results[0].works[2].worksPhoto;
+            var works3 = data.results[0].works[3].worksPhoto;
 
             $("span.englishName,h4.englishName").append(englishName);
             $("h3.position").append(position);
@@ -23,6 +24,8 @@ $(document).ready(function () {
             $("span.expertise").append(expertise);
             $("span.phone").append(phone);
             $("span.email").append(email);
+
+
         });
 
 });
