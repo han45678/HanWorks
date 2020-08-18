7bc236e9def726250ed903be5d9590e7a1793244
han45678 <han0910812144@gmail.com>
2020-08-18 21:16:46 +0800

    修改

--- js/ui.js
+++ js/ui.js
@@ -18,7 +18,14 @@ $(window).load(function () {
         prevArrow: $('#works  .prev'),
         responsive: [
             {
-                breakpoint: 767,
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 520,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
