41b15491ba144fdfbe890f0251e0befe5a02bdfb
han45678 <han0910812144@gmail.com>
2022-09-14 11:09:06 +0800

    Add files via upload

--- demo/map/map.js
+++ demo/map/map.js
@@ -64,20 +64,22 @@ var centerPoint = { //打卡的座標
 var m = 150 //打卡的範圍
 
 
-setTimeout(() => {
-    function arePointsNear(checkPoint, centerPoint, m) { // credits to user:69083
-        var km = m / 1000;
-        var ky = 40000 / 360;
-        var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
-        var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
-        var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
-        return Math.sqrt(dx * dx + dy * dy) <= km;
-    }
+function arePointsNear(checkPoint, centerPoint, m) { // credits to user:69083
+    var km = m / 1000;
+    var ky = 40000 / 360;
+    var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+    var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
+    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+    return Math.sqrt(dx * dx + dy * dy) <= km;
+}
+
+function punch(){
     if(arePointsNear(checkPoint, centerPoint, m)){
         alert("可以打卡")
     }else{
         alert("不可以打卡")
     }
-}, 1000);
+}
+
 
 
